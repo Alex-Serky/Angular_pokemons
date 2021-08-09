d4223012931e066814fa6429e843f95d2837543b
Alex-Serky <aserky@gmail.com>
2021-08-10 00:22:53 +0200

    Les Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found.component';
-
+import { PokemonsModule } from './pokemons/pokemons.module';
 @NgModule({
-  // déclarez BorderCardDirective dans le module racine de l'application
-  declarations: [
-    AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    PageNotFoundComponent
-  ],
+  // L'odre de chargement des modules est très important
+  // par rapport à l'ordre de déclaration des routes !
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PokemonsModule,
+    AppRoutingModule // pour l'ordre de déclaration des routes !
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-function appRoutes(): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
-  throw new Error('Function not implemented.');
-}
+
 
